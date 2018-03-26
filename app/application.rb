59452a01e3089d176f7a53ea4f3577add46d84eb
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-26 18:09:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,8 +16,10 @@ class Application
         resp.write "item not found"
         resp.status = 404
       end
+    else
+      resp.write "Route not found"
+      resp.status = 404
     end
-
     resp.finish
   end
 end
